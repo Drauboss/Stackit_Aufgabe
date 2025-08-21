f56822a184a1f0300329658faa83c0434e2db428
Berkay Yilmazer <berkay.yilmazer@gmail.com>
2025-08-21 12:50:30 +0200

    added the api code

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express, { Express, Request, Response } from 'express';
+
+interface Notification {
+    Type: 'Warning' | 'Info'; //can add more
+    Name: string;
+    Description: string;
+}
+
+const app: Express = express();
+const PORT: number = 3000;
+
+const notifications: Notification[] = [];
+
+app.use(express.json())
+
+//TODO: change to something more suitable
+function forwardToMessenger(notification: Notification): void {
+    console.log(notification)
+}
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'This is an API for forwarding messages to a messaging service' })
+})
+
+app.post('/notifications', (req: Request, res: Response) => {
+    const notification: Notification = req.body;
+
+    if (!notification || !notification.Type || !notification.Name || !notification.Description) {
+        return res.status(400).json({ error: 'Invalid Notification Body!' })
+    }
+
+    notifications.push(notification);
+
+    switch (notification.Type) {
+        case 'Warning':
+            forwardToMessenger(notification);
+            return res.status(200).json({ message: 'Notification will be forwarded.' })
+            break;
+        case 'Info':
+            return res.status(200).json({ message: 'Notification will not be forwarded.' })
+            break;
+        default:
+            return res.status(400).json({ message: 'Notification Type is not valid.' })
+            break;
+    }
+})
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+export default app;
